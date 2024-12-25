510304234ebff90e5e16e80416f842695352b548
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2024-12-25 13:46:50 +0100

    Log class

---
+++ LibraryManager_V2/Models/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager_V2.Models
+{
+    internal class Log
+    {
+        public int ID { get; set; }
+        public string Message { get; set; } = String.Empty;
+        public DateTime TimeStamp { get; set; }
+
+        public Log(int id, string message, DateTime timeStamp)
+        {
+            ID = id;
+            Message = message;
+            TimeStamp = timeStamp;
+        }
+    }
+}
