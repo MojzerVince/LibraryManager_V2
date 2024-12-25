652ca7c41c3ac546b00c843b0e2423265a75ca49
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2024-12-25 14:01:08 +0100

    main

--- LibraryManager_V2/Program.cs
+++ LibraryManager_V2/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LibraryManager_V2
+﻿using LibraryManager_V2.Models;
+using LibraryManager_V2.Repositories;
+using LibraryManager_V2.Services;
+
+namespace LibraryManager_V2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var service = new LibraryService(new BookRepository());
+
+            service.AddBook(new Book(
+                0,
+                "The Hobbit",
+                "J.R.R. Tolkien",
+                Category.Fantasy,
+                5
+            ));
         }
     }
 }
