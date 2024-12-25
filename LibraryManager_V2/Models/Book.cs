588cd242a12cfc2c5ebd6a49ed0114eb0b4a7a0c
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2024-12-25 13:13:03 +0100

    added categories to book class

--- LibraryManager_V2/Models/Book.cs
+++ LibraryManager_V2/Models/Book.cs
@@ -6,20 +6,46 @@ using System.Threading.Tasks;
 
 namespace LibraryManager_V2.Models
 {
+    public enum Category
+    {
+        Fantasy,
+        ScienceFiction,
+        Mystery,
+        Romance,
+        Horror,
+        Thriller,
+        HistoricalFiction,
+        NonFiction,
+        Biography,
+        Autobiography,
+        Poetry,
+        Drama,
+        Comedy,
+        Satire,
+        Tragedy,
+        Action,
+        Adventure,
+        Fable,
+        FairyTale,
+        Mythology,
+        Legend,
+        Science,
+    }
+
     internal class Book
     {
         public int ID { get; set; }
         public string Title { get; set; } = String.Empty;
         public string Author { get; set; } = String.Empty;
-        public string ReleaseDate { get; set; } = String.Empty;
+        public Category Genre { get; set; }
         public int Quantity { get; set; }
 
-        public Book(int id, string title, string author, string releaseDate, int quantity)
+        public Book(int id, string title, string author, Category genre, int quantity)
         {
             ID = id;
             Title = title;
             Author = author;
-            ReleaseDate = releaseDate;
+            Genre = genre;
             Quantity = quantity;
         }
     }
