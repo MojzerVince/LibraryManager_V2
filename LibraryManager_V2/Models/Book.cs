24f9fa514c2461ed4b41612c22fcce94b55f2012
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2024-12-25 12:57:11 +0100

    Book class

---
+++ LibraryManager_V2/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager_V2.Models
+{
+    internal class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Author { get; set; } = String.Empty;
+        public string ReleaseDate { get; set; } = String.Empty;
+        public int Quantity { get; set; }
+
+        public Book(int id, string title, string author, string releaseDate, int quantity)
+        {
+            ID = id;
+            Title = title;
+            Author = author;
+            ReleaseDate = releaseDate;
+            Quantity = quantity;
+        }
+    }
+}
