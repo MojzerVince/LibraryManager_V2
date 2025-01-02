5a3c10e98f4a11726c6a976024b17f29ecd32c5e
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2025-01-02 16:16:31 +0100

    student exec and working book lending

---
+++ LibraryManager_V2_Student/Program.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManager_V2.Repositories;
+using LibraryManager_V2.Services;
+using LibraryManager_V2_Student.Services;
+
+namespace LibraryManager_V2_Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ConsoleService(new LibraryService(new BookRepository()));
+
+            service.Run();
+        }
+    }
+}
---
+++ LibraryManager_V2_Student/Services/ConsoleService.cs
@@ -0,0 +1,99 @@
+﻿using LibraryManager_V2.Models;
+using LibraryManager_V2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager_V2_Student.Services
+{
+    internal class ConsoleService
+    {
+        private LibraryService service;
+
+        public ConsoleService(LibraryService service)
+        {
+            this.service = service;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Welcome to the Library!");
+            Console.ResetColor();
+
+            //TEST ONLY
+            service.AddBook(new Book("The Hobbit", "J.R.R. Tolkien", Category.Fantasy, 5));
+            service.AddBook(new Book("The Lord of the Rings", "J.R.R. Tolkien", Category.Fantasy, 3));
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("\nPlease select an option:");
+            Console.WriteLine("0. Books");
+            Console.WriteLine("1. Lend a book");
+            Console.WriteLine("2. Return a book");
+            Console.WriteLine("3. Message to admins");
+            Console.WriteLine("4. Exit\n");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "0":
+                    GetAllBooks();
+                    Run();
+                    break;
+                case "1":
+                    LendBook();
+                    break;
+                case "2":
+                    //ReturnBook();
+                    break;
+                case "3":
+                    //SendMessage();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write("Invalid option. Please try again.");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                    Run();
+                    break;
+            }
+        }
+
+        private void GetAllBooks()
+        {
+            Console.Clear();
+            foreach (Book b in service.rep.GetAllBooks())
+            {
+                Console.Write($"{b.ID} | ");
+                Console.Write($"{b.Title} | ");
+                Console.Write($"{b.Author} | ");
+                Console.Write($"{b.Genre} | ");
+                Console.Write($"{b.Quantity} left\n");
+            }
+        }
+
+        private void LendBook()
+        {
+            GetAllBooks();
+            Console.Write("\nPlease enter the ID of the book you want to lend: ");
+            int id = int.Parse(Console.ReadLine());
+            if(service.rep.GetBookById(id) != null)
+                service.LendBook(service.rep.GetBookById(id));
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write("Invalid ID. Please try again.");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+            Run();
+        }
+    }
+}
