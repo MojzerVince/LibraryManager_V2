694842ad767ad6aed8758ac6d588cff368f148ab
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2025-01-05 19:22:14 +0100

    added tostring override and changed Date and Message order

--- LibraryManager_V2/Models/Log.cs
+++ LibraryManager_V2/Models/Log.cs
@@ -9,14 +9,19 @@ namespace LibraryManager_V2.Models
     public class Log
     {
         public int ID { get; set; }
-        public string Message { get; set; } = String.Empty;
         public DateTime Date { get; set; }
+        public string Message { get; set; } = String.Empty;
 
-        public Log(int id, string message, DateTime date)
+        public Log(int id, DateTime date, string message)
         {
             ID = id;
-            Message = message;
             Date = date;
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return $"{ID}|{Date}|{Message}";
         }
     }
 }
