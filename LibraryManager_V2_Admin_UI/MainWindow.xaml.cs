d3f251f0d7f44761b591ba53a53d30502b4c00a5
MojziPC Blathy 212 <mojzer.vince@blathy.info>
2025-04-07 13:26:39 +0200

    add book is working in WPF application

---
+++ LibraryManager_V2_Admin_UI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using LibraryManager_V2.Services;
+using LibraryManager_V2.Repositories;
+using LibraryManager_V2.Models;
+
+namespace LibraryManager_V2_Admin_UI
+{
+    public partial class MainWindow : Window
+    {
+        LibraryService libraryService = new LibraryService(new BookRepository());
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            var categories = Enum.GetValues(typeof(Category)).Cast<Category>();
+            foreach (var category in categories)
+            {
+                Genres.Items.Add(new ComboBoxItem { Content = category.ToString() });
+            }
+        }
+
+        private void Integer(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(cc => Char.IsNumber(cc));
+            base.OnPreviewTextInput(e);
+        }
+
+        private void AddBook(object sender, RoutedEventArgs e)
+        {
+            Book b = new Book(Title.Text, Author.Text, (Category)Genres.SelectedIndex, Int32.Parse(Quantity.Text));
+            libraryService.AddBook(b);
+            MessageBox.Show("Book added successfully!");
+        }
+    }
+}
