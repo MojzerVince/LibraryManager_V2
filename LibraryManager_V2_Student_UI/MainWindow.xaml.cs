103aafc1b658a3ba144ad4c1293767c3a69ba01d
Mojzi BlathyPC207 <mojzer.vince@blathy.info>
2025-04-07 12:27:31 +0200

    almost working book add

--- LibraryManager_V2_Student_UI/MainWindow.xaml.cs
+++ LibraryManager_V2_Student_UI/MainWindow.xaml.cs
@@ -9,13 +9,40 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LibraryManager_V2_Student_UI
+using LibraryManager_V2.Services;
+using LibraryManager_V2.Repositories;
+using LibraryManager_V2.Models;
+
+namespace LibraryManager_V2_Admin_UI
 {
     public partial class MainWindow : Window
     {
+        LibraryService libraryService = new LibraryService(new BookRepository());
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            var categories = Enum.GetValues(typeof(Category)).Cast<Category>();
+            foreach (var category in categories)
+            {
+                Genres.Items.Add(new ComboBoxItem { Content = category.ToString() });
+            }
+        }
+
+        private void Integer(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(cc => Char.IsNumber(cc));
+            base.OnPreviewTextInput(e);
+        }
+
+        private void AddBook(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
