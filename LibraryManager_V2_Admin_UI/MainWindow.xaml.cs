8f9827724cb752af46d6ad49502fc4814c62fd67
MojziPC Blathy 212 <mojzer.vince@blathy.info>
2025-04-07 14:03:06 +0200

    load book cards to screen

--- LibraryManager_V2_Admin_UI/MainWindow.xaml.cs
+++ LibraryManager_V2_Admin_UI/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace LibraryManager_V2_Admin_UI
         {
             InitializeComponent();
             LoadCategories();
+            libraryService.LoadBooks();
+            LoadBooks();
         }
 
         private void LoadCategories()
@@ -31,6 +33,44 @@ namespace LibraryManager_V2_Admin_UI
             foreach (var category in categories)
             {
                 Genres.Items.Add(new ComboBoxItem { Content = category.ToString() });
+            }
+        }
+
+        private void LoadBooks()
+        {
+            var books = libraryService.rep.GetAllBooks();
+            foreach (var book in books)
+            {
+                //StackPanel
+                StackPanel bookCard = new StackPanel();
+                bookCard.Margin = new Thickness(10);
+                bookCard.Background = new SolidColorBrush(Colors.Gray);
+
+                //Title
+                TextBlock title = new TextBlock { Text = book.Title, FontSize = 16, Foreground = new SolidColorBrush(Colors.White), HorizontalAlignment = HorizontalAlignment.Center };
+                bookCard.Children.Add(title);
+
+                //Author
+                TextBlock author = new TextBlock { Text = book.Author, FontSize = 16, Foreground = new SolidColorBrush(Colors.White), HorizontalAlignment = HorizontalAlignment.Center };
+                bookCard.Children.Add(author);
+
+                //Category
+                TextBlock category = new TextBlock { Text = book.Genre.ToString(), FontSize = 16, Foreground = new SolidColorBrush(Colors.White), HorizontalAlignment = HorizontalAlignment.Center };
+                bookCard.Children.Add(category);
+
+                //Quantity
+                TextBlock quantity = new TextBlock { Text = book.Quantity.ToString(), FontSize = 16, Foreground = new SolidColorBrush(Colors.White), HorizontalAlignment = HorizontalAlignment.Center };
+                bookCard.Children.Add(quantity);
+
+                //Edit Button
+                Button editButton = new Button { Content = "Edit", Width = 100, Height = 20, Margin = new Thickness(5), HorizontalAlignment = HorizontalAlignment.Center };
+                bookCard.Children.Add(editButton);
+
+                //Delete Button
+                Button deleteButton = new Button { Content = "Delete", Width = 100, Height = 20, Margin = new Thickness(5), HorizontalAlignment = HorizontalAlignment.Center };
+                bookCard.Children.Add(deleteButton);
+
+                BooksView.Children.Add(bookCard);
             }
         }
 
