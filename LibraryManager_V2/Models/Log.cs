3b4b2fdcd0472564488fce937976b1d729741c47
Mojzi BlathyPC 113 <mojzer.vince@blathy.info>
2025-05-16 08:37:47 +0200

    modified log class

--- LibraryManager_V2/Models/Log.cs
+++ LibraryManager_V2/Models/Log.cs
@@ -5,17 +5,19 @@
         public int ID { get; set; }
         public DateTime Date { get; set; }
         public string Message { get; set; } = String.Empty;
+        public string User { get; set; } = String.Empty;
 
-        public Log(int id, DateTime date, string message)
+        public Log(int id, DateTime date, string message, string user)
         {
             ID = id;
             Date = date;
             Message = message;
+            User = user;
         }
 
         public override string ToString()
         {
-            return $"{ID}|{Date}|{Message}";
+            return $"{ID}|{Date}|{User}|{Message}";
         }
     }
 }
