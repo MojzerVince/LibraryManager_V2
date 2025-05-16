b0cf68c4a5e001a0bcf37cb111f18579c2c386ab
Mojzi BlathyPC 113 <mojzer.vince@blathy.info>
2025-05-16 08:34:19 +0200

    removed unused usings

--- LibraryManager_V2/Models/Log.cs
+++ LibraryManager_V2/Models/Log.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibraryManager_V2.Models
+﻿namespace LibraryManager_V2.Models
 {
     public class Log
     {
